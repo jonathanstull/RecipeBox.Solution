7bc1ea1b01a5db06e91712fd183f8b5235d1669b
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-06-01 11:45:27 -0700

    apply context templates and tweak for model properties Co-authored-by: Michael Burton <micbur1@gmail.com> Co-authored-by: Sammai Gutierrez <sammaigtz@hotmail.com>

---
+++ RecipeBox/Models/RecipeBoxContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RecipeBox.Models
+{
+  public class RecipeBoxContext : IdentityDbContext<ApplicationUser>
+  {
+    public virtual DbSet<Ingredient> Ingredients { get; set; }
+    public DbSet<Recipe> Recipes { get; set; }
+    public DbSet<Rating> Ratings { get; set; }
+    public DbSet<IngredientRecipe> IngredientRecipe { get; set; }
+    public DbSet<RatingRecipe> RatingRecipes { get; set; }
+
+    public RecipeBoxContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
